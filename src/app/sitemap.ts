22f090d05f7f2c9ccd7767e18b407b7096cff0b3
kevin-van-deventer <kevinlenovomail@gmail.com>
2026-04-29 10:38:36 +0700

    robots and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://neon-code-studio.com",
+      url: "https://fullwebdevkev.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
